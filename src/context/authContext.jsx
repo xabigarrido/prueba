2b7085f1c20d8aa34989d0829e49914da9b2123a
xabigarrido <xabigarrido@gmail.com>
2025-02-01 13:58:36 +0100

    ee

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -29,10 +29,7 @@ export const AuthProvider = ({ children }) => {
   const singin = async (user) => {
     try {
       const res = await loginRequest(user);
-      const miCokkie = Cookies.set("token", "your_token_value", {
-        expires: 1,
-        path: "",
-      });
+
       console.log(miCokkie);
       console.log(res);
       setOnline(true);
